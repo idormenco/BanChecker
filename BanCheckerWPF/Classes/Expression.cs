4019d7a3f65ac7b1d12db745a6ff539fef779fd8
manolimihai@gmail.com <manolimihai@gmail.com>
2014-04-01 19:29:19 +0300

    Added Step Class

--- BanCheckerWPF/Classes/Expression.cs
+++ BanCheckerWPF/Classes/Expression.cs
@@ -16,13 +16,26 @@ namespace BanCheckerWPF.Classes
             X = x;
         }
 
+        public Expression(Step step, string entity, Action action, object x)
+        {
+            CurrentStep = step;
+            Entity = entity;
+            Action = action;
+            X = x;
+        }
+
+        public Step CurrentStep { get; set; }
         public string Entity { get; set; }
         public Action Action { get; set; }
         public object X { get; set; }
 
         public override string ToString()
         {
-            return Entity + " " + Action + " " + X;
+         /*   if(CurrentStep.FirstParentStep != 0 && CurrentStep.SecondParentStep != 0)
+                return CurrentStep.CurrentStep + ":" + " from " + CurrentStep.FirstParentStep + " and " + CurrentStep.SecondParentStep + " " + Entity + " " + Action + " " + X;
+            else
+                return CurrentStep.CurrentStep + ":" + " from " + CurrentStep.FirstParentStep + " " + Entity + " " + Action + " " + X;*/
+            return  Entity + " " + Action + " " + X;
         }
     }
 
